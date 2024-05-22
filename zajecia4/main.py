ffbe639bd3537019bb7a8391c4ac6a20bc423d7f
Kuba <kuba020307@gmail.com>
2024-05-22 17:08:23 +0200

    zadanie 4

--- zajecia4/main.py
+++ zajecia4/main.py
@@ -1,3 +1,4 @@
+
 prices_in_dollars = [2.40, 999, 34.21, 150, 1001]
 def change_to_dollars(price):
     price = str(price)
@@ -6,3 +7,4 @@ def change_to_dollars(price):
 result = list(map(change_to_dollars, prices_in_dollars))
 
 print(result)
+
